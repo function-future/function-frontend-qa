f738a791a3c173efa3a39a522b7ab4ee169326bc
karnandohuang <karnandohuang@outlook.com>
2019-12-15 18:07:25 +0700

    added url constant

--- src/main/java/com/future/function/qa/model/UrlConstant.java
+++ src/main/java/com/future/function/qa/model/UrlConstant.java
@@ -3,6 +3,8 @@ package com.future.function.qa.model;
 public interface UrlConstant {
 
   String HOME_URL = "http://localhost:10001";
+  String STICKY_NOTES_URL = "http://localhost:10001/sticky-notes";
+  String EDIT_STICKY_NOTES_URL = "http://localhost:10001/sticky-notes/edit";
   String JUDGING_LIST_URL = "https://localhost:10001/final-judgings";
   String ADD_JUDGING_URL = "http://localhost:10001/batches/{1}/final-judging/add";
   String JUDGING_DETAIL_URL = "http://localhost:10001/batches/{1}/final-judgings/{2}/detail";
